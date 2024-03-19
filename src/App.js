8f044f564163e2bcc503d88e5a7af85f959e6b39
Arul <arul.anthony@digivationworld.com>
2024-03-19 12:01:55 +0530

    working on changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { Routes,Route,Link } from 'react-router-dom';
 import Home from './page/Home';
 import About from './page/About';
+import Vans from './page/Vans';
+import "./server.js"
  
 
 function App() {
@@ -11,11 +13,13 @@ function App() {
         <Link className='site-logo' to="/">#vanlife</Link>
         <nav>  
           <Link to="/about">About</Link>
+          <Link to ="/van">Van</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />        
-        <Route path="/about" element={<About />} />               
+        <Route path="/about" element={<About />} />      
+        <Route path="/van" element={<Vans />} />               
       </Routes>
     </>
   );
