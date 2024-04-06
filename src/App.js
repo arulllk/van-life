a4b65a5012a1b1b3c6e3cba5ba97facc9fd4e62b
Arul <arul.anthony@digivationworld.com>
2024-04-06 22:07:44 +0530

    working on files

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import './App.css';
 import { Routes,Route,Link } from 'react-router-dom';
 import Home from './page/Home';
 import About from './page/About';
-import Vans from './page/Vans';
+import Vans from './page/Vans/Vans.jsx';
 import "./server.js"
+import VanDetail from './page/Vans/VanDetail.jsx';
+import Layout from './components/Layout.jsx';
+import Dashboard from './page/Host/Dashboard.jsx';
+import Income from './page/Host/Income.jsx';
+import Reviews from './page/Host/Reviews.jsx';
+import HostLayout from './components/HostLayout.jsx';
  
 
 function App() {
   return (
-    <>
-      <header>
-        <Link className='site-logo' to="/">#vanlife</Link>
-        <nav>  
-          <Link to="/about">About</Link>
-          <Link to ="/van">Van</Link>
-        </nav>
-      </header>
+    <>     
       <Routes>
-        <Route path="/" element={<Home />} />        
-        <Route path="/about" element={<About />} />      
-        <Route path="/van" element={<Vans />} />               
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />        
+          <Route path="about" element={<About />} />      
+          <Route path="vans" element={<Vans />} />     
+          <Route path="vans/:id" element={<VanDetail />} />     
+
+          <Route path="host" element={<HostLayout />} >
+            <Route index element={<Dashboard />} />    
+            <Route path="income" element={<Income />} />    
+            <Route path="reviews" element={<Reviews />} />     
+          </Route>       
+        </Route>
       </Routes>
     </>
   );
